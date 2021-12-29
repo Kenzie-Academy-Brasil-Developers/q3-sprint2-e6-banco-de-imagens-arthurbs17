a7839b70ac710b8c6b72bc9fd6ad8e9d8d4efba8
Arthur Sarmento <arthurbs17@gmail.com>
2021-12-29 17:47:04 -0300

    File not allowed message included

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 from .kenzie import create_directories, receive_file, files_directory, allowed_extensions, max_content_length
-from werkzeug.exceptions import Conflict, RequestEntityTooLarge, NotFound
+from werkzeug.exceptions import Conflict, RequestEntityTooLarge
 import os
 
 app = Flask(__name__)
@@ -17,6 +17,7 @@ def upload_file():
         return{"message": "Arquivo já existente!"}
     except(RequestEntityTooLarge):
         return{"message": "Arquivo excede o tamanho permitido(1mb)!"}
-    
+    except(FileNotFoundError):
+        return{"message": "Extensão não permitida nesse banco!"}
     else:
         return jsonify(file_received), 201
